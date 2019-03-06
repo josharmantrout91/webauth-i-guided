51233947a548fec5d9700e47fb17ec7b63c572b6
josharmantrout91 <josh.armantrout@gmail.com>
2019-03-06 12:53:29 -0500

    refactored file structure

---
+++ auth/auth-router.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const router = require("express").Router();
+const Users = require("./users/users-model.js");
+
+server.post("/api/register", (req, res) => {
+  let user = req.body;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post("/api/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = router;
---
+++ notes.md
@@ -0,0 +1,14 @@
+##JWT
+
+##SERVER
+
+- produce the token
+- sending the token to the client
+- read, decode, and verify the token
+- makes the payload avail to the rest of the api
+
+##CLIENT
+
+- Store the token
+- Send the token on every request
+- destroying the token on Logout
